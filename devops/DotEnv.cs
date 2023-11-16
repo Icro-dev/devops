fae1da2e6d2db74ebef7944dac6ee27a6719fa76
icro <icrolux@gmail.com>
2023-11-16 07:04:16 +0100

    first commit

---
+++ devops/DotEnv.cs
@@ -0,0 +1,54 @@
+﻿namespace devops;
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+    public class DotEnv
+    {
+        private string filePath;
+        private Dictionary<string, string> variables = new Dictionary<string, string>();
+
+        public DotEnv(string filePath)
+        {
+            this.filePath = filePath;
+            Load();
+        }
+
+        public string Get(string key)
+        {
+            if (!variables.ContainsKey(key))
+            {
+                throw new KeyNotFoundException($"Key '{key}' not found in .env file");
+            }
+
+            return variables[key];
+        }
+
+        private void Load()
+        {
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException($"File '{filePath}' not found");
+            }
+
+            foreach (string line in File.ReadAllLines(filePath))
+            {
+                if (line.StartsWith("#"))
+                {
+                    continue; // Ignore comments
+                }
+
+                string[] parts = line.Split('=');
+                if (parts.Length != 2)
+                {
+                    throw new FormatException($"Invalid format of line '{line}'");
+                }
+
+                string key = parts[0].Trim();
+                string value = parts[1].Trim();
+
+                variables[key] = value;
+            }
+        }
+    }
---
+++ devops/Program.cs
@@ -0,0 +1,96 @@
+using devops;
+using devops.Areas.Identity;
+using devops.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+// load .env file
+var root = Directory.GetCurrentDirectory();
+Debug.Print(root);
+var dotenvPath = Path.Combine(root, ".env");
+var dotenv = new DotEnv(dotenvPath);
+string Datasource = dotenv.Get("hostname");
+string User_Id = dotenv.Get("username");
+string Password = dotenv.Get("password");
+string Database = dotenv.Get("database");
+
+var connectionString = "Data Source=" + Datasource + ";" +
+                       "User ID=" + User_Id + ";" +
+                       "Password=" + Password + ";" +
+                       "Database=" + Database + ";" +
+                       "TrustServerCertificate=true";
+
+Debug.Print(connectionString);
+// Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+var app = builder.Build();
+
+
+/*var connectionString = "Data Source=" + env["hostname"] + ";" +
+                       "User ID=" + env["USERNAME"] + ";" +
+                       "Password=" + env["password"] + ";" +
+                       "Database=" + env["database"] + ";" +
+                       "TrustServerCertificate=true";*/
+
+var config =
+    new ConfigurationBuilder()
+        .AddEnvironmentVariables()
+        .Build();
+
+var env = Environment.GetEnvironmentVariables();
+
+
+//Migration and seeding data
+using (var scope = app.Services.CreateScope())
+{
+    //running migrations at startup
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+    //adding seeddata
+
+    //   var services = scope.ServiceProvider;
+    // SeedData.Initialize(services);
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+//app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
